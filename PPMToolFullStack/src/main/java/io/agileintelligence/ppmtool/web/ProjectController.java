6ed75ffd9ee4f8b6d48d2b30fe9127e0916feee2
Bora Guven <guvenbe@gmail.com>
2019-06-29 20:58:26 -0400

    Added service controller repository erorr handling abd vlidation

---
+++ PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/web/ProjectController.java
@@ -0,0 +1,40 @@
+package io.agileintelligence.ppmtool.web;
+
+
+import io.agileintelligence.ppmtool.Services.ProjectService;
+import io.agileintelligence.ppmtool.domain.Project;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/project")
+public class ProjectController {
+
+
+    private final MapValidationErrorService mapValidationErrorService;
+    private final ProjectService projectService;
+
+    public ProjectController(MapValidationErrorService mapValidationErrorService, ProjectService projectService) {
+        this.mapValidationErrorService = mapValidationErrorService;
+        this.projectService = projectService;
+    }
+
+    @PostMapping("")
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
+
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationErrorService(result);
+
+        if(errorMap != null) return errorMap;
+
+        Project project1 = projectService.saveOrUpdateProject(project);
+
+        return new ResponseEntity<Project>(project, HttpStatus.CREATED);
+    }
+}

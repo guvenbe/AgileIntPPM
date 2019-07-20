e863a0e48c8b7d320652035df3a054d328368e3e
Bora Guven <guvenbe@gmail.com>
2019-07-20 11:34:16 -0400

    Reducer redux installed with some init code

--- ppmtool-react-client/src/store.js
+++ ppmtool-react-client/src/store.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
-const initalState = {};
+const initalState = {} ;
 const middleware = [thunk];
 
 let store;

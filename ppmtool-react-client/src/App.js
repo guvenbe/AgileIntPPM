8e6e43203b5e125497e6063a9a58f4460be82f51
Bora Guven <guvenbe@gmail.com>
2019-08-21 16:21:37 -0400

    able to login

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,13 +1,12 @@
-
 import React, { Component } from "react";
-import './App.css';
+import "./App.css";
 import Dashboard from "./components/Dashboard";
-import Header from "./components/Layout/header";
-import "bootstrap/dist/css/bootstrap.min.css"
-import {BrowserRouter as Router, Route} from "react-router-dom"
+import Header from "./components/Layout/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import AddProject from "./components/Project/AddProject";
-import {Provider} from "react-redux"
-import store from "./store"
+import { Provider } from "react-redux";
+import store from "./store";
 import UpdateProject from "./components/Project/UpdateProject";
 import ProjectBoard from "./components/ProjectBoard/ProjectBoard";
 import AddProjectTask from "./components/ProjectBoard/ProjectTasks/AddProjectTask";
@@ -15,7 +14,26 @@ import UpdateProjectTask from "./components/ProjectBoard/ProjectTasks/UpdateProj
 import Landing from "./components/Layout/Landing";
 import Register from "./components/UserManagement/Register";
 import Login from "./components/UserManagement/Login";
+import jwt_decode from "jwt-decode";
+import setJWTToken from "./SecurityUtils/setJWTToken";
+import { SET_CURRENT_USER } from "./actions/types";
 
+const jwtToken = localStorage.jwtToken;
+
+if (jwtToken) {
+    setJWTToken(jwtToken);
+    const decoded_jwtToken = jwt_decode(jwtToken);
+    store.dispatch({
+        type: SET_CURRENT_USER,
+        payload: decoded_jwtToken
+    });
+
+    const currentTime = Date.now() / 1000;
+    if (decoded_jwtToken.exp < currentTime) {
+        //handle logout
+        //window.location.href = "/";
+    }
+}
 
 class App extends Component {
     render() {
@@ -23,22 +41,32 @@ class App extends Component {
             <Provider store={store}>
                 <Router>
                     <div className="App">
-                        <Header/>
+                        <Header />
                         {
                             //Public Routes
                         }
-                        <Route exact path="/" component={Landing}/>
-                        <Route exact path="/register" component={Register}/>
-                        <Route exact path="/login" component={Login}/>
+
+                        <Route exact path="/" component={Landing} />
+                        <Route exact path="/register" component={Register} />
+                        <Route exact path="/login" component={Login} />
+
                         {
                             //Private Routes
                         }
-                        <Route exact path="/dashboard" component={Dashboard}/>
-                        <Route exact path="/addProject" component={AddProject}/>
-                        <Route exact path="/updateProject/:id" component={UpdateProject}/>
-                        <Route exact path="/projectBoard/:id" component={ProjectBoard}/>
-                        <Route exact path="/addProjectTask/:id" component={AddProjectTask}/>
-                        <Route exact path="/updateProjectTask/:backlog_id/:pt_id" component={UpdateProjectTask}/>
+                        <Route exact path="/dashboard" component={Dashboard} />
+                        <Route exact path="/addProject" component={AddProject} />
+                        <Route exact path="/updateProject/:id" component={UpdateProject} />
+                        <Route exact path="/projectBoard/:id" component={ProjectBoard} />
+                        <Route
+                            exact
+                            path="/addProjectTask/:id"
+                            component={AddProjectTask}
+                        />
+                        <Route
+                            exact
+                            path="/updateProjectTask/:backlog_id/:pt_id"
+                            component={UpdateProjectTask}
+                        />
                     </div>
                 </Router>
             </Provider>
